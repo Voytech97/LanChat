fd7c594b9ff18018362a0d0a1176b1582ed969ec
Wojciech 165431 <chx104461@student.chorzow.merito.pl>
2026-09-01 20:45:18 +0200

    Initial commit

---
+++ LanChat.Android/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia;
+using Avalonia.Android;
+
+namespace LanChat.Android;
+
+[Activity(
+    Label = "LanChat.Android",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity
+{
+}
