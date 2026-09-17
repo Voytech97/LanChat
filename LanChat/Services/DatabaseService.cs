9bc9444f3687eb99c6213f94b5488e37ee74b894
Wojciech 165431 <chx104461@student.chorzow.merito.pl>
2026-09-17 19:06:10 +0200

    added DatabaseService initialization and table schemes

--- LanChat/Services/DatabaseService.cs
+++ LanChat/Services/DatabaseService.cs
@@ -1,10 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using Microsoft.Data.Sqlite;
 
-namespace LanChat.Services
+namespace LanChat.Services;
+public class DatabaseService
 {
-    internal class DatabaseService
+    private readonly string _connectionString;
+    public DatabaseService()
     {
+        // creates folder on users disk in appdata/local/lanchat
+        var folder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        var appFolder = Path.Combine(folder, "LanChat");
+        Directory.CreateDirectory(appFolder);
+
+        var dbPath = Path.Combine(appFolder, "lanchat_client.db");
+        _connectionString = $"Data Source={dbPath}";
+
+        InitializeDatabase();
+    }
+
+    private void InitializeDatabase()
+    {
+        using var connection = new SqliteConnection(_connectionString);
+        connection.Open();
+        var command = connection.CreateCommand();
+        //Account table: holds your login and private key
+        //Message table: holds your messages history
+        command.CommandText = @"
+            CREATE TABLE IF NOT EXISTS Account (
+                Username TEXT PRIMARY KEY,
+                PrivateKey TEXT NOT NULL
+            );
+
+            CREATE TABLE IF NOT EXISTS Messages (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Sender TEXT NOT NULL,
+                Receiver TEXT NOT NULL,
+                Content TEXT NOT NULL,
+                Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                IsMine INTEGER NOT NULL
+            );
+        ";
+        command.ExecuteNonQuery();
+
     }
 }
+
